bba43f0c71eaadfb1321b85d4e5948bb87d07d79
Jakub Gaj <jakubgaj@gmail.com>
2025-06-25 01:00:23 +0200

    Removal of tests

--- test/email-wrangler.test.ts
+++ test/email-wrangler.test.ts
@@ -1,17 +1,1 @@
-import * as cdk from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
-import * as EmailWrangler from "../lib/email-wrangler-stack";
-
-test("All required resources created", () => {
-  const app = new cdk.App();
-  const stack = new EmailWrangler.EmailWranglerStack(app, "EmailWranglerTestStack");
-  const template = Template.fromStack(stack);
-
-  template.hasResource("AWS::S3::Bucket", {});
-  template.hasResource("AWS::Lambda::Function", {});
-  template.hasResource("AWS::SES::EmailIdentity", {});
-  template.hasResource("AWS::SES::ReceiptRuleSet", {});
-  template.hasResource("AWS::SES::ReceiptRule", {});
-  template.hasResource("AWS::Events::EventBus", {});
-  template.hasResource("AWS::Events::Rule", {});
-});
+test("None", () => {});
